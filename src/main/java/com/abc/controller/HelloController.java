6c790326df3730375e1b704818aaa0746725639f
bansyad <justice4luvnwar@gmail.com>
2020-02-19 13:45:28 -0500

    halde

---
+++ src/main/java/com/abc/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.abc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	
+	
+	@RequestMapping(value = "/chiki", method= RequestMethod.GET )
+	public String helloController() {
+		return "hello";
+		
+
+
+		
+		
+	}
+
+}
---
+++ src/main/webapp/page/hello.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<h2>inside page ok hello java!</h2>
+</body>
+</html>
